9b2429140702d669339daf2879bc19b74e2cf175
trevor wells <104236134+trevor-wells@users.noreply.github.com>
2023-05-02 16:23:09 -0600

    final commit

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens = [int for int in num_list if int % 2 == 0]
+    return evens
 
 def make_exclamation(sentence_list):
-    pass
+    exclamations = [str + "!" for str in sentence_list]
+    return exclamations
